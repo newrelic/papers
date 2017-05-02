53cd22879eeed1644389919a8bbbaed64f32a2d0
Jason R. Clark <jclark@newrelic.com>
2017-05-02 09:30:24 -0700

    Revising help to use options more conventionally

--- lib/papers/cli.rb
+++ lib/papers/cli.rb
@@ -5,43 +5,50 @@ module Papers
   class CLI
 
     def run
-      if options[:generate]
-        begin
-          Papers::ManifestGenerator.new.generate!
-        rescue Papers::FileExistsError => e
-          warn "Error: 'papers_manifest.yml' already exists at '#{e.message}'. Aborting..."
-        end
-      end
+      options.parse!
 
-      if options[:update]
+      case @command
+      when :generate
+        Papers::ManifestGenerator.new.generate!
+      when :update
         Papers::ManifestUpdater.new.update!
+      when :help
+        emit_help ""
+      else
+        emit_help "Unrecognized command. Must run either"
       end
+    rescue Papers::FileExistsError => e
+      warn "Error: 'papers_manifest.yml' already exists at '#{e.message}'. Aborting..."
+    rescue OptionParser::ParseError => e
+      emit_help "Problem parsing options: #{e.message}"
     end
 
     private
 
     def options
-      @options ||= parse_options
-    end
-
-    def parse_options
-      options = {}
-      OptionParser.new do |opts|
+      @options ||= OptionParser.new do |opts|
         opts.banner = 'Usage: papers [options]'
 
         opts.on('-g', '--generate', 'Generate papers_manifest.yml') do |v|
-          options[:generate] = v
+          @command = :generate
         end
 
         opts.on("-u", "--update", "Update papers_manifest.yml for Rubygems") do |v|
-          options[:update] = v
+          @command = :update
         end
 
-        opts.on_tail('-h', '--help', 'Display this screen') do
-          puts opts
+        opts.on('-h', '--help', 'Display this screen') do
+          @command = :help
         end
-      end.parse!
-      options
+      end
+    end
+
+    def emit_help(header)
+      unless header.empty?
+        puts header
+        puts
+      end
+      puts options
     end
   end
 end
